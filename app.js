0b5d777dca767354380b3c9010524e9768a6fbcb
Adam <xpingmp@gmail.com>
2024-02-02 18:21:45 -0700

    API changes to express

--- app.js
+++ app.js
@@ -1,28 +1,25 @@
-var server = require("http").createServer();
+var express = require('express');
+var bodyParser = require('body-parser');
 
-server.on("request", (request, response) => {
-    var body = [];
-    request.on("data", chunk => {
-        body.push(chunk);
-    });
-    request
-        .on("end", () => {
-            let bodyString = body.concat().toString();
-            console.log(bodyString);
-            response.end(bodyString);
-        })
-        .on("error", () => {
-            response.statusCode = 400;
-            response.end();
-        });
-    response.on("error", err => {
-        console.error(err);
-    });
-});
-server.listen(process.env.PORT || 8008, () => {
-    console.log("Server listening at 8008");
+var server = express();
+server.use(bodyParser.text({
+    type: function(req) {
+        return 'text';
+    }
+}));
+
+server.post("/", (req, res)=>{
+    console.log('Got: ' + req.body);
+    res = res.status(200);
+    var contentType = req.get('Content-Type');
+    if (contentType) {
+        console.log("Content Type: " + contentType);
+        res = res.type(contentType);
+    }
+    res.send(req.body);
 });
 
-module.exports = server; // for testing
+server.listen(process.env.PORT || 8080);
 
-//curl -d "echo" -H "Content-Type: text" -X POST http://localhost:8008
+module.exports = server;
+
